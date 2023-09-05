8a076d9d0fdf7555defe6268059416e86c668093
steveabecassis <abecassis.steve@gmail.com>
2023-09-05 23:41:34 +0300

    modify esm files add padding=True

--- ESMFold.sh
+++ ESMFold.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem=10G
 
 mkdir output/esm_fold_output
-python3 ./ESMFoldHF.py ./2QKEE_002.a3m  ./output/esm_fold_output/ -name 'test'
+python3 ./ESMFoldHF.py -input ./2QKEE_002.a3m  -output ./output/esm_fold_output/ -name 'test'
