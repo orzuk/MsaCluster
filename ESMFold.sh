5794f3b6c360e035a6b8654a7cede5d3b7950aba
steveabecassis <abecassis.steve@gmail.com>
2023-09-19 22:38:35 +0300

    modify esmfold sh file

--- ESMFold.sh
+++ ESMFold.sh
@@ -7,5 +7,4 @@
 
 mkdir output/esm_fold_output
 python3 ./ESMFoldHF.py -input ./msas4esm/cluster_001.a3m -output ./output/esm_fold_output -name 'Cluster001_'
-python3 ./ESMFoldHF.py -input ./msas4esm/cluster_013.a3m -output ./output/esm_fold_output -name 'Cluster013_'
 
