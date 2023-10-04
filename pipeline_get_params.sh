b6b005f1c1bb9d048acdf47394a4624c22c3860a
orzuk <or.zuk@mail.huji.ac.il>
2023-10-04 19:12:13 +0300

    Modified output jobs

--- pipeline_get_params.sh
+++ pipeline_get_params.sh
@@ -14,8 +14,8 @@ python3 ./get_msa.py $FASTA_FILE_INPUT ./Pipeline/$OUTPUT_NAME_DIR/output_get_ms
 mkdir -p Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster
 python3  ./ClusterMSA_moriah.py --keyword ShallowMsa -i ./Pipeline/$OUTPUT_NAME_DIR/output_get_msa/DeepMsa.a3m  -o ./Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/AF_preds
-sbatch -o $OUTPUT_NAME_DIR/RunAF.out  ./Pipeline/RunAF_params.sh $OUTPUT_NAME_DIR
+sbatch -o ./Pipeline/$OUTPUT_NAME_DIR/RunAF.out  ./Pipeline/RunAF_params.sh $OUTPUT_NAME_DIR
 #mkdir -p ./Pipeline/output/esm_fold_output
-#sbatch -o $OUTPUT_NAME_DIR/ESMFold.out ./Pipeline/RunEsmFold.sh
+#sbatch -o ./Pipeline/$OUTPUT_NAME_DIR/ESMFold.out ./Pipeline/RunEsmFold.sh
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_cmap_esm
-sbatch -o $OUTPUT_NAME_DIR/CmapESM.out  ./CmapESM_params.sh $OUTPUT_NAME_DIR
+sbatch -o ./Pipeline/$OUTPUT_NAME_DIR/CmapESM.out  ./CmapESM_params.sh $OUTPUT_NAME_DIR
