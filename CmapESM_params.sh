05709671f25983514b88d26c5e0c571f11d576fe
Or Zuk <orzuk@cs.huji.ac.il>
2023-10-04 18:15:35 +0300

    removed environment

--- CmapESM_params.sh
+++ CmapESM_params.sh
@@ -6,8 +6,6 @@
 
 OUTPUT_NAME_DIR="$1"
 
-source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
-. /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate
 module load torch/1.3
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/esm_cmap_output
 python3  ./runESM.py  --input_msas ./Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster -o ./Pipeline/$OUTPUT_NAME_DIR/output_cmap_esm
