c1daa81c707f3aad2c0e3294d8373163ec71bba1
orzuk <or.zuk@mail.huji.ac.il>
2023-10-04 17:06:36 +0300

    Updated shell scripts path

--- CmapESM_params.sh
+++ CmapESM_params.sh
@@ -7,6 +7,7 @@
 OUTPUT_NAME_DIR="$1"
 
 source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
+. /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate
 module load torch/1.3
-mkdir /sci/labs/orzuk/steveabecassis/MsaCluster/Pipeline/$OUTPUT_NAME_DIR/esm_cmap_output
-python3  ./runESM.py  --input_msas /sci/labs/orzuk/steveabecassis/MsaCluster/Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster -o /sci/labs/orzuk/steveabecassis/MsaCluster/Pipeline/$OUTPUT_NAME_DIR/output_cmap_esm
+mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/esm_cmap_output
+python3  ./runESM.py  --input_msas ./Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster -o ./Pipeline/$OUTPUT_NAME_DIR/output_cmap_esm
--- pipeline_get_params.sh
+++ pipeline_get_params.sh
@@ -7,15 +7,15 @@
 FASTA_FILE_INPUT="$1"
 OUTPUT_NAME_DIR="$2"
 
-mkdir Pipeline
-mkdir Pipeline/$OUTPUT_NAME_DIR
-mkdir Pipeline/$OUTPUT_NAME_DIR/output_get_msa
+mkdir -p Pipeline
+mkdir -p Pipeline/$OUTPUT_NAME_DIR
+mkdir -p Pipeline/$OUTPUT_NAME_DIR/output_get_msa
 python3 ./get_msa.py $FASTA_FILE_INPUT ./Pipeline/$OUTPUT_NAME_DIR/output_get_msa  -name 'DeepMsa'
-mkdir Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster
+mkdir -p Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster
 python3  ./ClusterMSA_moriah.py --keyword ShallowMsa -i ./Pipeline/$OUTPUT_NAME_DIR/output_get_msa/DeepMsa.a3m  -o ./Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster
-mkdir ./Pipeline/$OUTPUT_NAME_DIR/AF_preds
+mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/AF_preds
 sbatch ./Pipeline/RunAF_params.sh $OUTPUT_NAME_DIR
-#mkdir /sci/labs/orzuk/steveabecassis/MsaCluster/Pipeline/output/esm_fold_output
+#mkdir -p /sci/labs/orzuk/steveabecassis/MsaCluster/Pipeline/output/esm_fold_output
 #sbatch /sci/labs/orzuk/steveabecassis/MsaCluster/Pipeline/RunEsmFold.sh
-mkdir  ./Pipeline/$OUTPUT_NAME_DIR/output_cmap_esm
+mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_cmap_esm
 sbatch ./CmapESM_params.sh $OUTPUT_NAME_DIR
