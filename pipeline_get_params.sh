fc4ae6c8d9c8ce61a0a9a47d9d2ed0c97f306088
orzuk <or.zuk@mail.huji.ac.il>
2023-10-07 20:26:40 +0300

    set pipeline with jobs

--- pipeline_get_params.sh
+++ pipeline_get_params.sh
@@ -13,9 +13,10 @@ mkdir -p Pipeline/$OUTPUT_NAME_DIR/output_get_msa
 python3 ./get_msa.py $FASTA_FILE_INPUT ./Pipeline/$OUTPUT_NAME_DIR/output_get_msa  -name 'DeepMsa'
 mkdir -p Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster
 python3  ./ClusterMSA_moriah.py --keyword ShallowMsa -i ./Pipeline/$OUTPUT_NAME_DIR/output_get_msa/DeepMsa.a3m  -o ./Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster
+
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/AF_preds
 sbatch -o ./Pipeline/$OUTPUT_NAME_DIR/RunAF.out  ./Pipeline/RunAF_params.sh $OUTPUT_NAME_DIR
 #mkdir -p ./Pipeline/output/esm_fold_output
 #sbatch -o ./Pipeline/$OUTPUT_NAME_DIR/ESMFold.out ./Pipeline/RunEsmFold.sh
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_cmap_esm
-sbatch -o ./Pipeline/$OUTPUT_NAME_DIR/CmapESM.out  ./CmapESM_params.sh $OUTPUT_NAME_DIR
+sbatch -o ./Pipeline/$OUTPUT_NAME_DIR/CmapESM.out  ./Pipeline/CmapESM_params.sh $OUTPUT_NAME_DIR
