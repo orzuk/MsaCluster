ab8a52d5c84969acf284204e5b315584959c203f
orzuk <or.zuk@mail.huji.ac.il>
2023-10-08 17:53:19 +0300

    Updated environment

--- Pipeline/CmapESM_params.sh
+++ Pipeline/CmapESM_params.sh
@@ -10,8 +10,6 @@ OUTPUT_NAME_DIR="$1"
 
 . /sci/labs/orzuk/orzuk/my-python-venv/bin/activate
 
-source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
-. /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate
 module load torch/1.3
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/esm_cmap_output
 python3  ./runESM.py  --input_msas ./Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster -o ./Pipeline/$OUTPUT_NAME_DIR/output_cmap_esm
