82007ac99b6f7ea4d5980cd4ca13a4975b2525b7
orzuk <or.zuk@mail.huji.ac.il>
2023-10-14 16:50:29 +0300

    Added msa prints

--- get_msa.py
+++ get_msa.py
@@ -22,17 +22,29 @@ from argparse import ArgumentParser
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("input",default="input",help="Should be a .fasta file")
+    parser.add_argument("input", default="input",help="Should be a .fasta file")
     parser.add_argument("results", help="Directory to write the results to")
     parser.add_argument("-name", help="msa name")
     # parser.add_argument("--msa-mode",default="mmseqs2_uniref_env",choices=["mmseqs2_uniref_env","mmseqs2_uniref","single_sequence"],help="Choose the Db" )
     args = parser.parse_args()
     result_dir = Path(args.results)
     queries, is_complex = get_queries(args.input, 'random')
+    print("queries:")
+    print(queries)
+    print("is_complex:")
+    print(is_complex)
     sequence = queries[0][1]
+    print("sequence:")
+    print(sequence)
     (unpaired_msa, paired_msa, query_seqs_unique, query_seqs_cardinality, template_features) = \
         get_msa_and_templates(query_sequences=sequence, jobname=args.name, result_dir= result_dir,
                               use_templates=False, custom_template_path='',pair_mode='', msa_mode='mmseqs2_uniref_env')
+    print("unpaired msa:")
+    print(unpaired_msa)
+    print("paired msa:")
+    print(paired_msa)
+    print("query seqs unique:")
+    print(query_seqs_unique)
     msa = msa_to_str(unpaired_msa, paired_msa, query_seqs_unique, query_seqs_cardinality)
     result_dir.joinpath(f"{args.name}.a3m").write_text(msa)
 
