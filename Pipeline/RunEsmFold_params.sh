140c9ef76e71f31b687718b772e6fb31fae54c3c
orzuk <or.zuk@mail.huji.ac.il>
2023-11-27 19:22:35 +0200

    Added ESMFold script

---
+++ Pipeline/RunEsmFold_params.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --time=05:00:00
+#SBATCH --ntasks=8
+#SBATCH --mem=32G
+
+#SBATCH --partition=puffin
+#SBATCH --gres=gpu:a30
+
+
+OUTPUT_NAME_DIR="$1"
+
+
+RUN_OPERATION="$2"
+
+
+. /sci/labs/orzuk/orzuk/my-python-venv/bin/activate
+
+module load torch/1.3
+mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_cmap_esm
+
+python3  ./ESMFoldHF.py  -input ./Pipeline/$OUTPUT_NAME_DIR/output_msa_cluster -o ./Pipeline/$OUTPUT_NAME_DIR/output_esm_fold
+
