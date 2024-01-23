9a1406833ea2446b0ccdb9ffe48aea74f47690ff
steveabecassis <abecassis.steve@gmail.com>
2024-01-23 11:37:16 +0200

    change venv in cmap esm

--- Pipeline/CmapESM_params.sh
+++ Pipeline/CmapESM_params.sh
@@ -14,7 +14,7 @@ OUTPUT_NAME_DIR="$1"
 RUN_OPERATION="$2"
 
 
-. /sci/labs/orzuk/orzuk/my-python-venv/bin/activate
+source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
 
 module load torch/1.3
 mkdir -p $OUTPUT_NAME_DIR/output_cmap_esm
