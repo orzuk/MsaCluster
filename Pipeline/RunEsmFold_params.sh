60c43385de30d6ddeac24053bccf78aa576d245d
steveabecassis <abecassis.steve@gmail.com>
2024-02-09 13:06:57 +0200

    modifification for esm fold

--- Pipeline/RunEsmFold_params.sh
+++ Pipeline/RunEsmFold_params.sh
@@ -9,7 +9,7 @@
 OUTPUT_NAME_DIR="$1"
 
 
-. /sci/labs/orzuk/orzuk/my-python-venv/bin/activate
+source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
 
 module load torch/1.3
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_esm_fold
