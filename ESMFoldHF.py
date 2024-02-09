1ba560ae475834abdfdaac209f89b18cd0b7a512
steveabecassis <abecassis.steve@gmail.com>
2024-02-09 13:02:01 +0200

    modifification for esm fold

--- ESMFoldHF.py
+++ ESMFoldHF.py
@@ -131,3 +131,15 @@ if __name__ == '__main__':
         path = f'./Pipeline/{fold_pair}'
         seq_fold1 = extract_protein_sequence(f'{path}/chain_pdb_files/{fold1}.pdb')
         seq_fold2 = extract_protein_sequence(f'{path}/chain_pdb_files/{fold2}.pdb')
+
+        inputs = tokenizer([seq_fold1], return_tensors="pt", add_special_tokens=False, padding=True)
+        outputs = model(**inputs)
+        folded_positions = outputs.positions
+        pdb = convert_outputs_to_pdb(outputs)
+        save_string_as_pdb(pdb[0], f'./Pipeline/{fold_pair}/esm_fold_output/{fold1}_esm.pdb')
+
+        inputs = tokenizer([seq_fold2], return_tensors="pt", add_special_tokens=False, padding=True)
+        outputs = model(**inputs)
+        folded_positions = outputs.positions
+        pdb = convert_outputs_to_pdb(outputs)
+        save_string_as_pdb(pdb[0], f'./Pipeline/{fold_pair}/esm_fold_output/{fold2}_esm.pdb')
