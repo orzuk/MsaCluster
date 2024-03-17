29636d94a110dcc2100a36c458b4b56f6976a842
steveabecassis <abecassis.steve@gmail.com>
2024-03-17 21:00:40 +0200

    Adjust GPU memory

--- Pipeline/RunEsmFold_params.sh
+++ Pipeline/RunEsmFold_params.sh
@@ -12,7 +12,7 @@ OUTPUT_NAME_DIR="$1"
 source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
 
 module load torch/1.3
-mkdir -p ./$OUTPUT_NAME_DIR/output_esm_fold
+mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_esm_fold
 
 python3  ./ESMFoldHF.py  -input $OUTPUT_NAME_DIR
 
