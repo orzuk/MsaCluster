c33763c5500fef18eeab7e9d9cd6383d33d7d260
steveabecassis <abecassis.steve@gmail.com>
2024-03-17 21:30:30 +0200

    Adjust GPU memory

--- ESMFoldHF.py
+++ ESMFoldHF.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
     print('############################################################################')
 
 
-    if len(seq_fold2)
+    # if len(seq_fold2)
     inputs = tokenizer([seq_fold2], return_tensors="pt", add_special_tokens=False, padding=True).to(device)
     outputs = model(**inputs)
     folded_positions = outputs.positions
