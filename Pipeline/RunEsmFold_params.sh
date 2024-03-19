01e5a9674ff330dd321b5511b75503ecdce137c5
steveabecassis <abecassis.steve@gmail.com>
2024-03-19 20:52:26 +0200

    Adjust GPU memory

--- Pipeline/RunEsmFold_params.sh
+++ Pipeline/RunEsmFold_params.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=8
 #SBATCH --mem=24G
 #SBATCH --partition=dogfish
-#SBATCH --gres=gpu:a100:3
+#SBATCH --gres=gpu:a100:2
 
 OUTPUT_NAME_DIR="$1"
 
@@ -13,7 +13,7 @@ source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
 
 module load torch/1.3
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_esm_fold
-
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256
 python3  ./ESMFoldHF.py  -input $OUTPUT_NAME_DIR
 
 
