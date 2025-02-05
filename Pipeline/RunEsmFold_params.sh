0c423addd4e7fd8e1249ddcf6916839356b3ba2e
steveabecassis <abecassis.steve@gmail.com>
2025-02-05 20:33:59 +0200

    debug sh file

--- Pipeline/RunEsmFold_params.sh
+++ Pipeline/RunEsmFold_params.sh
@@ -4,14 +4,15 @@
 #SBATCH --ntasks=8
 #SBATCH --mem=24G
 
+OUTPUT_NAME_DIR="$1"
 
 
+.  /sci/labs/orzuk/steveabecassis/steve_venv/bin/activate
 
-#. /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate
-#source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
-#module load torch/1.3
-#mkdir -p ./Pipeline/2pbkB_3njqA/output_esm_fold
-#export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256
-python3  ./ESMFoldHF.py  -input 2pbkB_3njqA
+
+mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_esm_fold
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256
+python3  ./ESMFoldHF.py  -input $OUTPUT_NAME_DIR
+
 
 
