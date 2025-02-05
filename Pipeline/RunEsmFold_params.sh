34a0ce66e41b3ebb7d5d9fa7ef2a3aef25084053
steveabecassis <abecassis.steve@gmail.com>
2025-02-05 14:37:20 +0200

    adjust sh file

--- Pipeline/RunEsmFold_params.sh
+++ Pipeline/RunEsmFold_params.sh
@@ -3,8 +3,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=8
 #SBATCH --mem=24G
-#SBATCH --partition=dogfish
-#SBATCH --gres=gpu:a100:2
+
 
 OUTPUT_NAME_DIR="$1"
 
@@ -15,4 +14,5 @@ OUTPUT_NAME_DIR="$1"
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_esm_fold
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256
 python3  ./ESMFoldHF.py  -input $OUTPUT_NAME_DIR
+
 
