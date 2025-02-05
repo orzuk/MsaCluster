3fe442c03e39cf277605e3df782ec2b98faffe5f
steveabecassis <abecassis.steve@gmail.com>
2025-02-05 14:34:07 +0200

    adjust sh file

--- Pipeline/RunEsmFold_params.sh
+++ Pipeline/RunEsmFold_params.sh
@@ -8,9 +8,9 @@
 
 OUTPUT_NAME_DIR="$1"
 
-csh
-source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
 
+./sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
+#source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
 #module load torch/1.3
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_esm_fold
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256
