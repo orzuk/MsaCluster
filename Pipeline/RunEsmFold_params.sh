aaa3dbde53d892dae3f97803d4ca41e65b1a4be6
steveabecassis <abecassis.steve@gmail.com>
2025-02-05 14:46:33 +0200

    adjust sh file

--- Pipeline/RunEsmFold_params.sh
+++ Pipeline/RunEsmFold_params.sh
@@ -8,7 +8,7 @@
 OUTPUT_NAME_DIR="$1"
 
 
-#./sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
+. /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate
 #source /sci/labs/orzuk/steveabecassis/colabfold_new/bin/activate.csh
 #module load torch/1.3
 mkdir -p ./Pipeline/$OUTPUT_NAME_DIR/output_esm_fold
