689919dd17f8e003cc3cc1a9531b4fd247f2f390
orzuk <or.zuk@mail.huji.ac.il>
2025-09-22 12:35:00 +0300

    Fixed plots

--- utils/utils.py
+++ utils/utils.py
@@ -164,7 +164,8 @@ def pair_str_to_tuple(s: str) -> tuple[str, str]:
     """
     '1dzlA_5keqF' -> ('1dzlA','5keqF')
     """
-    if isinstance(s, tuple): # ready correct format
+    print("Stripping to tupple: ", s)
+    if isinstance(s, tuple) or isinstance(s, list): # ready correct format
         return s
     s = s.strip()
     if "_" not in s:
