ed8d9ed564b5acd11c8e7a81dbf42464ed267dd7
orzuk <or.zuk@mail.huji.ac.il>
2025-09-30 09:48:11 +0300

    Update cmaps

--- runMSATrans.py
+++ runMSATrans.py
@@ -16,105 +16,124 @@ mpl.use("agg")
 torch.set_grad_enabled(False)
 
 
-# Subsampling MSA
+if __name__ == '__main__':
 
-if __name__=='__main__':
-
-    p = argparse.ArgumentParser(description=
-    """
+    p = argparse.ArgumentParser(description="""
     Run esm-1b and MSA transformer model.
     """)
-
-    p.add_argument("--input_msas", nargs='*', action='store',help='Path to msas to use in prediction.')
-    p.add_argument("-o", action="store", help='name of output directory to write contact maps to.')
-    p.add_argument("-saveformat", action="store", help='output file format (text or pickle).')
-    p.add_argument("--model", action='store', default='msa_t', help="Model: `esm1b` or `msa_t` (default is 'msa_t')")
-    p.add_argument('--keyword', action='store', default='', help="Keyword for this prediction")
-    p.add_argument("--test", action='store_true', help='Tests first 3 constructs.')
-    p.add_argument("--parallel", action='store_true', help='Runs in parallel using Pandarallel.')
-
+    p.add_argument("--input_msas", nargs='*', action='store',
+                   help='Path to MSAs to use in prediction (dir with *.a3m or list of *.a3m files).')
+    p.add_argument("-o", action="store", help='Output directory for contact maps.')
+    p.add_argument("-saveformat", action="store", help='Output format: text or pickle (default: .npy).')
+    p.add_argument("--model", action='store', default='msa_t',
+                   help="Model: `esm1b` or `msa_t` (default: 'msa_t').")
+    p.add_argument('--keyword', action='store', default='', help="Keyword tag for this prediction.")
+    p.add_argument("--test", action='store_true', help='Test mode: only first 3 inputs.')
+    p.add_argument("--parallel", action='store_true', help='(unused) Runs in parallel using Pandarallel.')
     args = p.parse_args()
 
-
+    # --- I/O setup ---
     os.makedirs(args.o, exist_ok=True)
-    args.test = False
-    if args.test:
-        args.input_msas = args.input_msas[:3]
-
-    # msa_cluster_path = args.input_msas
-    # input_msas = [f'{msa_cluster_path}' + msa for msa in os.listdir(msa_cluster_path) if 'a3m' in str(msa)]
-    #
-    # msas = {
-    #     os.path.basename(msa_fil).replace('.a3m',''): read_msa(msa_fil)
-    #     for msa_fil in input_msas
-    # }
-    print('####')
-    print(args.input_msas)
-    print('####')
+    print('####'); print(args.input_msas); print('####')
     print("Is running with GPU? " + str(torch.cuda.is_available()))
-
     start_time = time.time()
+
+    if not args.input_msas:
+        raise ValueError("No --input_msas provided.")
+
+    # Accept either a single directory or explicit .a3m files
+    if len(args.input_msas) == 1 and os.path.isdir(args.input_msas[0]):
+        input_dir = args.input_msas[0]
+        input_a3ms = sorted(glob(os.path.join(input_dir, "*.a3m")))
+    else:
+        input_a3ms = [p for p in args.input_msas if p.endswith(".a3m")]
+        # also allow a directory accidentally mixed in the list
+        extra_dirs = [d for d in args.input_msas if os.path.isdir(d)]
+        for d in extra_dirs:
+            input_a3ms.extend(sorted(glob(os.path.join(d, "*.a3m"))))
+        input_a3ms = sorted(set(input_a3ms))
+
+    if args.test:
+        input_a3ms = input_a3ms[:3]
+
+    # Read cluster MSAs
     msas = {
-        os.path.basename(msa_fil).replace('.a3m', ''): read_msa(args.input_msas[0] + '/'+ msa_fil)
-        for msa_fil in os.listdir(args.input_msas[0])
+        os.path.splitext(os.path.basename(msa_fil))[0]: read_msa(msa_fil)
+        for msa_fil in input_a3ms
     }
 
-    # New! insert to MSA also the full alignment (calculating cmap can take long for this one)
-    msas["MSA_deep"] = read_msa(args.input_msas[0].replace(
-        "output_msa_cluster", "output_get_msa/DeepMsa.a3m"))
+    # Derive pair root to locate DeepMsa.* robustly (works for .../output_msa_cluster and .../output_msa_cluster_pairtrim)
+    if len(args.input_msas) == 1 and os.path.isdir(args.input_msas[0]):
+        base = args.input_msas[0]
+    else:
+        base = os.path.dirname(input_a3ms[0]) if input_a3ms else "."
+    pair_root = (base
+                 .replace("/output_msa_cluster_pairtrim", "")
+                 .replace("\\output_msa_cluster_pairtrim", "")
+                 .replace("/output_msa_cluster", "")
+                 .replace("\\output_msa_cluster", ""))
 
-    # msas = {
-    #     os.path.basename(msa_fil).replace('.a3m',''): read_msa(msa_fil)
-    #     for msa_fil in args.input_msas
-    # }
+    deep_candidates = [
+        os.path.join(pair_root, "output_get_msa", "DeepMsa_pairtrim.a3m"),  # preferred
+        os.path.join(pair_root, "output_get_msa", "DeepMsa.a3m_pairtrim"),  # legacy alt
+        os.path.join(pair_root, "output_get_msa", "DeepMsa.a3m"),           # original
+    ]
+    for deep_path in deep_candidates:
+        if os.path.isfile(deep_path):
+            print(f"[MSAT] Using Deep MSA: {deep_path}")
+            msas["MSA_deep"] = read_msa(deep_path)
+            break
 
-    sequences = {
-        name: msa[0] for name, msa in msas.items()
-    }
+    # For esm1b we need single sequences (first row of each MSA)
+    sequences = {name: msa[0] for name, msa in msas.items()}
 
+    # --- Load model ---
     if args.model == 'esm1b':
         mdl, mdl_alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
     elif args.model == 'msa_t':
         mdl, mdl_alphabet = esm.pretrained.esm_msa1b_t12_100M_UR50S()
     else:
-        print('model not understood.')
+        raise ValueError(f"Unknown --model '{args.model}' (expected 'esm1b' or 'msa_t').")
 
     mdl = mdl.eval()
     batch_converter = mdl_alphabet.get_batch_converter()
 
-    # Remove old cmaps in the same directory!
+    # Remove old outputs (npy files) in the target directory
     print("Removing old files: " + args.o + '/*.npy')
-    print(glob(args.o + '/*.npy'))
-    for f_old in glob(args.o + '/*.npy'):
-        os.remove(f_old)
+    old = glob(os.path.join(args.o, '*.npy'))
+    print(old)
+    for f_old in old:
+        try:
+            os.remove(f_old)
+        except OSError:
+            pass
 
+    # --- Predict ---
     if args.model == 'esm1b':
         for name, inputs in sequences.items():
-            batch_labels,batch_strs,batch_tokens = batch_converter([inputs])
+            batch_labels, batch_strs, batch_tokens = batch_converter([inputs])
             batch_tokens = batch_tokens.to(next(mdl.parameters()).device)
             print('ESM1b predicting...')
-            pred = mdl.predict_contacts(batch_tokens)[0].cpu()
-            pred = pred.detach().cpu().numpy()
-            print(np.sum(pred))
+            pred = mdl.predict_contacts(batch_tokens)[0].detach().cpu().numpy()
             if args.saveformat == "text":
-                np.savetxt("%s/%s_%s_%s.npy" % (args.o, args.model, args.keyword, name), pred)
-            else:  # Save compact form
-                np.save("%s/%s_%s_%s.npy" % (args.o, args.model, args.keyword, name), pred)
-            print("wrote %s/%s_%s_%s.npy" % (args.o, args.model, args.keyword, name))
+                np.savetxt(f"{args.o}/{args.model}_{args.keyword}_{name}.npy", pred)
+            else:
+                np.save(f"{args.o}/{args.model}_{args.keyword}_{name}.npy", pred)
+            print(f"wrote {args.o}/{args.model}_{args.keyword}_{name}.npy")
 
     elif args.model == 'msa_t':
-        for name, inputs in msas.items():
-            inputs = greedy_select(inputs, num_seqs=128) # can change this to pass more/fewer sequences
+        for name, msa_rows in msas.items():
+            # reduce MSA rows to a manageable depth (keeps first row as query)
+            inputs = greedy_select(msa_rows, num_seqs=128)
             batch_labels, batch_strs, batch_tokens = batch_converter([inputs])
             batch_tokens = batch_tokens.to(next(mdl.parameters()).device)
             print('MSA-Transformer predicting...')
-            pred = mdl.predict_contacts(batch_tokens)[0].cpu()
-            pred = pred.detach().cpu().numpy()
-            print(np.sum(pred))
+            pred = mdl.predict_contacts(batch_tokens)[0].detach().cpu().numpy()
             if args.saveformat == "text":
-                np.savetxt("%s/%s_%s_%s.npy" % (args.o, args.model, args.keyword, name), pred)
+                np.savetxt(f"{args.o}/{args.model}_{args.keyword}_{name}.npy", pred)
             else:
-                np.save("%s/%s_%s_%s.npy" % (args.o, args.model, args.keyword, name), pred)
-            print("wrote %s/%s_%s_%s.npy" % (args.o, args.model, args.keyword, name))
+                np.save(f"{args.o}/{args.model}_{args.keyword}_{name}.npy", pred)
+            print(f"wrote {args.o}/{args.model}_{args.keyword}_{name}.npy")
 
-    print("Finished! Runtime for " + str(len(msas.items())) + " alignments = " + str(time.time()-start_time) + " seconds")
+    print("Finished! Runtime for " + str(len(msas)) + " alignments = "
+          + str(time.time() - start_time) + " seconds")
