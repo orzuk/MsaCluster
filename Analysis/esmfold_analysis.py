0fa4c95f62827208c65daf3213fc617a7381eff7
Or Zuk <or.zuk@mail.huji.ac.il>
2025-10-14 17:49:29 +0300

    Fixed cmap analysis

--- Analysis/esmfold_analysis.py
+++ Analysis/esmfold_analysis.py
@@ -49,8 +49,8 @@ def compute_esmfold_pred_tmscores(fold_pair):
                 pred_pdb  = r["pdb_path"]
                 print("Compute TMscores for ESMFold PDB file: " + str(pred_pdb) + " ; " + str(ctr_pdb_file) + " out of " + str(num_esm_pdb_files))
                 ctr_pdb_file += 1
-                TMscore_fold1 = compute_tmscore_align(truth1_pdb, pred_pdb)
-                TMscore_fold2 = compute_tmscore_align(truth2_pdb, pred_pdb)
+                TMscore_fold1 = compute_tmscore_align(truth1_pdb, pred_pdb, chain1=chains[0])
+                TMscore_fold2 = compute_tmscore_align(truth2_pdb, pred_pdb, chain1=chains[0])
                 final_res.append({
                     "fold_pair": fold_pair,
                     "model": model_tag,
@@ -69,8 +69,8 @@ def compute_esmfold_pred_tmscores(fold_pair):
                 # infer a clean sample name (works with ShallowMsa_003__sample_007_esm2.pdb)
                 base = os.path.basename(pred_file)
                 pred_name = base.replace(f"_{model_tag}.pdb", "").replace(".pdb", "")
-                TMscore_fold1 = compute_tmscore_align(truth1_pdb, pred_path)
-                TMscore_fold2 = compute_tmscore_align(truth2_pdb, pred_path)
+                TMscore_fold1 = compute_tmscore_align(truth1_pdb, pred_path, chain1=chains[0])
+                TMscore_fold2 = compute_tmscore_align(truth2_pdb, pred_path, chain1=chains[0])
                 final_res.append({
                     "fold_pair": fold_pair,
                     "model": model_tag,
